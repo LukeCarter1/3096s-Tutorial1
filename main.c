d9eb2d0c67c002dc91ca59c469697d8b375dbd60
LukeCarter1 <luke.lcarter0210@gmail.com>
2023-08-04 22:51:34 +0200

    Fixed error in code where an a should have been a b

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
     printf("Enter a value for a: ");
     scanf("%d", &a);
     printf("Enter a value for b: ");    
-    scanf("%d", &a);
+    scanf("%d", &b);
     sum = a + b;
     printf("The sum of a and b is %d \n.",sum);
 }
